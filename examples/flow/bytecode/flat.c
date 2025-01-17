494d9bdb9b98967bc6c6ce3ed744dffb83430e2d
Steven Cassamajor <contributor@networkaxis.org>
2025-01-17 01:15:54 -0500

    Successfully compile `flow` example

--- examples/flow/bytecode/flat.c
+++ examples/flow/bytecode/flat.c
@@ -1,10 +1,22 @@
 //go:build ignore
-
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
 
 #include <linux/bpf.h>
+#include <linux/bpf_common.h>
 
+#include <linux/if_ether.h>
+#include <linux/if_packet.h>
+#include <linux/in.h>
+#include <linux/in6.h>
+#include <linux/ip.h>
+#include <linux/ipv6.h>
+#include <linux/tcp.h>
+#include <linux/udp.h>
+#include <linux/pkt_cls.h>
+
+#include <bpf/bpf_endian.h>
 #include <bpf/bpf_helpers.h>
 
 SEC("tc")
