92e2ef7c879f1648373d921d43ecd997fb1228a7
Steven Cassamajor <contributor@networkaxis.org>
2025-01-21 16:21:53 -0500

    Add checks for IPv4 and IPv6

--- examples/flow/bytecode/flat.c
+++ examples/flow/bytecode/flat.c
@@ -53,6 +53,92 @@ int flat(struct __sk_buff* skb) {
         return TC_ACT_OK;
     }
 
+    void* head = (void*)(long)skb->data; /* Start of packet data */ 
+    void* tail = (void*)(long)skb->data_end; /* End of packet data */ 
+
+    /* Ensure the start of our packet data plus the size of the Ethernet header
+       does not extend beyond the ned of the packet data. */ 
+    if (head + sizeof(struct ethhdr) > tail) {
+        return TC_ACT_OK;
+    }
+
+    struct ethhdr* eth = head;
+    struct iphdr* ip;
+    struct ipv6hdr* ipv6;
+    struct tcphdr* tcp;
+    struct udphdr* udp;
+
+    struct packet_t pkt = {0};
+    uint32_t offset = 0;
+
+    /* h_proto field represents the EtherType in the Ethernet header */
+    /* EtherType field determines the Layer 3 Protocol */
+    /* This field is Big Endian */
+    switch (bpf_ntohs(eth->h_proto)) {
+    case ETH_P_IP: /* IPv4 */
+        offset = sizeof(struct ethhdr) + sizeof(struct iphdr);
+
+        if (head + offset > tail) {
+            return TC_ACT_OK
+        }
+
+        ip = head + sizeof(struct ethhdr);
+
+        if (ip->protocol != IP_PROTO_TCP && ip->protocol != IPPROTO_UDP) {
+            return TC_ACT_OK;
+        }
+
+        /* Place the source and destination IP of our packet into the last index of `in6_addr */
+        pkt.src_ip.in6_u.u6_addr32[3] = ip->saddr;
+        pkt.dst_ip.in6_u.u6_addr32[3] = ip->daddr;
+
+        // Pad the 16 bites before IP address with all Fs as per the RFC
+        pkt.src_ip.in6_u.u6_addr32[5] = 0xffff;
+        pkt.dst_ip.in6_u.u6_addr32[5] = 0xffff;
+
+        pkt.protocol = ip->protocol;
+        pkt.ttl = ip->ttl;
+
+        break;
+
+    case ETH_P_IPV6: /* IPv6 */
+        offset = sizeof(struct ethhdr) + sizeof(struct ipv6hdr)
+
+        if (head + offset > tail) {
+            return TC_ACT_OK;
+        }
+
+        ipv6 = (void*)head + sizeof(struct ethhdr);
+
+        if (ipv6->nexthdr != IPPROTO_TCP || ipv6->nexthdr != IPPROTO_UDP) {
+            return TC_ACT_OK
+        }
+
+        pkt.src_ip = ipv6->saddr;
+        pkt.dst_ip = ipv6->daddr;
+
+        pkt.protocol = ipv6->nexthdr;
+        pkt.ttl = ipv6->hop_limit;
+
+        break;
+
+    default: // We did not have an IPv4 or IPv6 packet
+        return TC_ACT_OK;
+    }
+
+    /* Conduct a bound check to ensure the packet is either TCP or UDP */
+    if (head + offset + sizeof*(struct tcphdr) > tail || head + offset + sizeof(udphdr) > tail) {
+        return TC_ACT_OK;
+    }
+
+    /* Process TCP and UDP Segments*/
+    switch (pkt.protocol) {
+
+    }
+
+
+
+
 
     return 0;
 }
