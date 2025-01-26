15ae20442c45b6a6545fdca1f86cdd58fd96001e
Steven Cassamajor <contributor@networkaxis.org>
2025-01-26 14:59:51 -0500

    Move main to probe but keep main

--- examples/flow/probe/probe.go
+++ examples/flow/probe/probe.go
@@ -3,6 +3,7 @@ package probe
 import (
 	"log"
 
+	"github.com/cassamajor/xcnf/examples/flow/bytecode"
 	"golang.org/x/sys/unix"
 )
 
@@ -11,6 +12,16 @@ const (
 	twentyMB = tenMB * 2
 	fortyMB  = tenMB * 4
 )
+
+func loadObjects() (*bytecode.ProbeObjects, error) {
+	var objs bytecode.ProbeObjects
+
+	if err := bytecode.LoadProbeObjects(&objs, nil); err != nil {
+		return nil, err
+	}
+
+	return &objs, nil
+}
 
 func setRlimit() error {
 	log.Println("Setting rlimit")
