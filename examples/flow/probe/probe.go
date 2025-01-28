8a8de483de1ccceaf1082aa9d4da8706cde16aae
Steven Cassamajor <contributor@networkaxis.org>
2025-01-27 22:29:08 -0500

    Iterate on probes package

--- examples/flow/probe/probe.go
+++ examples/flow/probe/probe.go
@@ -4,6 +4,8 @@ import (
 	"log"
 
 	"github.com/cassamajor/xcnf/examples/flow/bytecode"
+	"github.com/cassamajor/xcnf/examples/flow/clsact"
+	"github.com/vishvananda/netlink"
 	"golang.org/x/sys/unix"
 )
 
@@ -13,14 +15,28 @@ const (
 	fortyMB  = tenMB * 4
 )
 
-func loadObjects() (*bytecode.ProbeObjects, error) {
+type probe struct {
+	iface      netlink.Link
+	handle     *netlink.Handle
+	qdisc      *clsact.ClsAct
+	bpfObjects *bytecode.ProbeObjects
+	filters    []*netlink.BpfFilter
+}
+
+// loadObjects instantiates an empty `ProbeObjects` struct that holds the eBPF program and maps.
+func (p *probe) loadObjects() error {
+	log.Printf("Loading probe object to kernel")
+
 	var objs bytecode.ProbeObjects
 
+	// Load the eBPF program into the kernel
 	if err := bytecode.LoadProbeObjects(&objs, nil); err != nil {
-		return nil, err
+		return err
 	}
 
-	return &objs, nil
+	p.bpfObjects = &objs
+
+	return nil
 }
 
 func setRlimit() error {
@@ -31,3 +47,36 @@ func setRlimit() error {
 		Max: fortyMB,
 	})
 }
+
+func newProbe(iface netlink.Link) (*probe, error) {
+	log.Println("Creating a new probe")
+
+	handle, err := netlink.NewHandle(unix.NETLINK_ROUTE)
+
+	if err != nil {
+		log.Printf("Failed getting netlink handle: %v", err)
+		return nil, err
+	}
+
+	hook := probe {
+		iface: iface,
+		handle: handle,
+	}
+	
+	if err := hook.loadObjects(); err != nil {
+		return nil, err
+	}
+
+	if err := hook.createQdisc(); err != nil {
+		log.Printf("Failed creating qdisc: %v", err)
+		return nil, err
+	}
+
+	if err := hook.createFilters(); err != nil {
+		log.Printf("Failed creating qdisc filters: %v", err)
+		return nil, err
+	}
+
+	return &hook, nil
+}
+
--- examples/flow/probe/probe_test.go
+++ examples/flow/probe/probe_test.go
@@ -1,28 +1,30 @@
 package probe
 
 import (
- "testing"
+	"testing"
 
- "github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"
 )
 
 func TestProbeLoad(t *testing.T) {
- _, err := loadObjects()
+	hook := probe{}
+    err := hook.loadObjects()
 
- require.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func TestPacket(t *testing.T) {
+	hook := probe{}
 
- objs, err := loadObjects()
+	err := hook.loadObjects()
 
- require.NoError(t, err)
+	require.NoError(t, err)
 
- in := []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5}
+	in := []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5}
 
- ret, out, err := objs.Flat.Test(in)
+	ret, out, err := hook.bpfObjects.Flat.Test(in)
 
- require.NoError(t, err)
- require.Equal(t, uint32(0), ret)
- require.Equal(t, in, out)
+	require.NoError(t, err)
+	require.Equal(t, uint32(0), ret)
+	require.Equal(t, in, out)
 }
