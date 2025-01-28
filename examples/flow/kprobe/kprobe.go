957ab1dc87aa792efae6d6d602400a75f6e7101f
Steven Cassamajor <contributor@networkaxis.org>
2025-01-28 00:01:37 -0500

    Continue iterating

--- examples/flow/kprobe/kprobe.go
+++ examples/flow/kprobe/kprobe.go
@@ -58,11 +58,11 @@ func newProbe(iface netlink.Link) (*probe, error) {
 		return nil, err
 	}
 
-	hook := probe {
-		iface: iface,
+	hook := probe{
+		iface:  iface,
 		handle: handle,
 	}
-	
+
 	if err := hook.loadObjects(); err != nil {
 		return nil, err
 	}
@@ -81,4 +81,71 @@ func newProbe(iface netlink.Link) (*probe, error) {
 }
 
 // createQdisc creates the `clasact` qdisc
-func (p *probe)
+func (p *probe) createQdisc() error {
+	log.Printf("Creating qdisc")
+
+	p.qdisc = clsact.NewClsAct(&netlink.QdiscAttrs{
+		LinkIndex: p.iface.Attrs().Index,
+		Handle:    netlink.MakeHandle(0xffff, 0),
+		Parent:    netlink.HANDLE_CLSACT,
+	})
+
+	if err := p.handle.QdiscAdd(p.qdisc); err != nil {
+		if err := p.handle.QdiscReplace(p.qdisc); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// createFilters creates BPF filters and classifiers
+func (p *probe) createFilters() error {
+	log.Printf("Creating qdisc filters")
+
+	p.addFilter(netlink.FilterAttrs{
+		LinkIndex: p.iface.Attrs().Index,
+		Handle:    netlink.MakeHandle(0xffff, 0),
+		Parent:    netlink.HANDLE_MIN_INGRESS,
+		Protocol:  unix.ETH_P_IP,
+	})
+
+	p.addFilter(netlink.FilterAttrs{
+		LinkIndex: p.iface.Attrs().Index,
+		Handle:    netlink.MakeHandle(0xffff, 0),
+		Parent:    netlink.HANDLE_MIN_EGRESS,
+		Protocol:  unix.ETH_P_IP,
+	})
+
+	p.addFilter(netlink.FilterAttrs{
+		LinkIndex: p.iface.Attrs().Index,
+		Handle:    netlink.MakeHandle(0xffff, 0),
+		Parent:    netlink.HANDLE_MIN_INGRESS,
+		Protocol:  unix.ETH_P_IPV6,
+	})
+
+	p.addFilter(netlink.FilterAttrs{
+		LinkIndex: p.iface.Attrs().Index,
+		Handle:    netlink.MakeHandle(0xffff, 0),
+		Parent:    netlink.HANDLE_MIN_EGRESS,
+		Protocol:  unix.ETH_P_IPV6,
+	})
+
+	for _, filter := range p.filters {
+		if err := p.handle.FilterAdd(filter); err != nil {
+			if err := p.handle.FilterReplace(filter); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// addFilter takes a netlink filter to build a BPF filter or classifiers, passing filter attributes to our program's file descriptor.
+func (p *probe) addFilter(attrs netlink.FilterAttrs) {
+	p.filters = append(p.filters, &netlink.BpfFilter{
+		FilterAttrs:  attrs,
+		Fd:           p.bpfObjects.ProbePrograms.Flat.FD(),
+		DirectAction: true,
+	})
+}
